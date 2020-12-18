b83f20c41c8c1d18f8d2047e63fb6360110c33d1
Markus Seidel <markus.seidel@cern.ch>
2020-12-18 11:13:30 +0100

    Configuration files

---
+++ configs/runMassWeights_WminusJToMuNu_cfg.py
@@ -0,0 +1,72 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Eras.Era_Run2_2016_cff import Run2_2016
+
+process = cms.Process('MassWeights',Run2_2016)
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10)
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+      '/store/mc/RunIISummer20UL16MiniAOD/WminusJetsToTauNu_TauToMu_TuneCP5_13TeV-powhegMiNNLO-pythia8-photos/MINIAODSIM/106X_mcRun2_asymptotic_v13-v2/00000/006B67C5-B922-D646-B0AC-9774918B1987.root'
+    ),
+    secondaryFileNames = cms.untracked.vstring()
+)
+
+process.options = cms.untracked.PSet()
+
+
+# Output definition
+
+process.MINIAODSIMoutput = cms.OutputModule("PoolOutputModule",
+    compressionAlgorithm = cms.untracked.string('LZMA'),
+    compressionLevel = cms.untracked.int32(4),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('MINIAODSIM'),
+        filterName = cms.untracked.string('')
+    ),
+    dropMetaData = cms.untracked.string('ALL'),
+    eventAutoFlushCompressedSize = cms.untracked.int32(-900),
+    fastCloning = cms.untracked.bool(False),
+    fileName = cms.untracked.string('file:SMP-RunIISummer20UL16MiniAOD-massWeights.root'),
+    outputCommands = cms.untracked.vstring(
+        'drop *',
+        'keep *_correctMassWeights_*_*',
+        'keep GenRunInfoProduct_*_*_*',
+        'keep LHERunInfoProduct_*_*_*',
+        'keep GenFilterInfo_*_*_*',
+        'keep GenLumiInfoHeader_*_*_*',
+        'keep GenLumiInfoProduct_*_*_*',
+    ),
+    overrideInputFileSplitLevels = cms.untracked.bool(True),
+    splitLevel = cms.untracked.int32(0)
+)
+
+
+process.correctMassWeights = cms.EDProducer("ExternalLHEProducer",
+    args = cms.vstring('/cvmfs/cms.cern.ch/phys_generator/gridpacks/slc6_amd64_gcc700/13TeV/powheg/Vj_NNLOPS/Wj_slc6_amd64_gcc700_CMSSW_10_2_23_WminusJToMuNu-suggested-nnpdf31-ncalls-doublefsr-q139-ckm-powheg-MiNNLO31-svn3756-ew-rwl6-j200-st2fix-ana-hoppetweights-ymax20-addmassweights.tgz'),
+    nEvents = cms.untracked.int32(-1),
+    numberOfParameters = cms.uint32(1),
+    outputFile = cms.string('cmsgrid_final.lhe'),
+    scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh'),
+    generateConcurrently = cms.untracked.bool(False),
+)
+process.RandomNumberGeneratorService.correctMassWeights = cms.PSet(
+  initialSeed = cms.untracked.uint32(234567),
+  engineName = cms.untracked.string('MixMaxRng')
+)
+
+
+process.lhe_Step = cms.Path(process.correctMassWeights)
+process.MINIAODSIMoutput_step = cms.EndPath(process.MINIAODSIMoutput)
+
+process.schedule = cms.Schedule(process.lhe_Step, process.MINIAODSIMoutput_step)
---
+++ crab_submit_files/crab_submit_runMassWeights_ZJToMuMu_APVpreVFP.py
@@ -0,0 +1,25 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+config.General.requestName = 'runMassWeights_ZJToMuMu_APVpreVFP'
+config.General.workArea = 'crab_projects'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.numCores = 1
+config.JobType.maxMemoryMB = 2000
+config.JobType.maxJobRuntimeMin = 2400
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = '../configs/runMassWeights_ZJToMuMu_cfg.py'
+config.JobType.allowUndistributedCMSSW = True
+
+config.Data.inputDataset = '/DYJetsToMuMu_M-50_TuneCP5_13TeV-powhegMiNNLO-pythia8-photos/RunIISummer20UL16MiniAODAPV-106X_mcRun2_asymptotic_preVFP_v8-v2/MINIAODSIM'
+config.Data.allowNonValidInputDataset = True
+
+config.Data.splitting = 'EventAwareLumiBased'
+config.Data.unitsPerJob = 20000
+config.Data.outLFNDirBase = '/store/group/cmst3/group/wmass/w-mass-13TeV/edmLHE' 
+config.Data.publication = True
+config.Data.outputDatasetTag = 'LHE_massWeights_APVpreVFP'
+
+config.Site.storageSite = 'T2_CH_CERN'
